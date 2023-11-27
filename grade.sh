ce3894f9d332599a698d1c30a2d9909f1b9a1e12
Hayden Kwok <hayden.kwok@gmail.com>
2023-11-27 11:32:27 -0800

    added grade

--- grade.sh
+++ grade.sh
@@ -8,9 +8,21 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+files=`find student-submission/*.java`
+
+if [[ -f $files ]]; 
+then
+    echo "Valid Submission"
+else 
+    exit
+fi
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar student-submission/*.java
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
